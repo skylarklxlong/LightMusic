d0218e7c570021362cc33f0ce6dcb728e038ddaa
Skylark <li_xuelong@126.com>
2018-01-29 16:23:53 +0800

    初始化仓库

---
+++ app/src/main/java/online/himakeit/lightmusic/SplashActivity.java
@@ -0,0 +1,19 @@
+package online.himakeit.lightmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @author：LiXueLong
+ * @date:2018/1/29-16:10
+ * @mail1：skylarklxlong@outlook.com
+ * @mail2：li_xuelong@126.com
+ * @des：MainActivity
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
